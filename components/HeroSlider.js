8323e2021fe2266ad14237b5929fc1a8137baa00
Jimmy Fellous <jimmyfellous@gmail.com>
2025-09-01 11:18:18 +0200

    Hero hauteur -2% sur mobile et tablette

--- components/HeroSlider.js
+++ components/HeroSlider.js
@@ -21,7 +21,7 @@ export default function HeroSlider() {
   }, [])
 
   return (
-    <div className="relative w-full h-[97vh] md:h-[107vh] lg:h-[110vh] -mt-16">
+    <div className="relative w-full h-[95vh] md:h-[105vh] lg:h-[110vh] -mt-16">
       <div className="absolute inset-0 overflow-hidden">
         {slides.map((src, idx) => (
           <div
