9b309cf6fd14f9282298e7941b7e400b21a6845a
Jimmy Fellous <jimmyfellous@gmail.com>
2025-10-03 12:26:45 +0200

    Add Google Search Console verification
    - Create HTML verification file: googleyuQxHfwQQ4-eTZZTe8wygA_0YOhiTOIoykZm0DAu0JU.html
    - Add meta verification tag in _document.js
    - Enable Google Search Console access for blondie.paris

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,7 @@ export default function Document() {
         <meta charSet="utf-8" />
         <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="google-site-verification" content="yuQxHfwQQ4-eTZZTe8wygA_0YOhiTOIoykZm0DAu0JU" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Lilita+One&display=swap" rel="stylesheet" />
